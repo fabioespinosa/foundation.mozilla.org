c0f084a25adf9b752ffe7252361c5d245ab24694
Simon Fessehaye <fessehay@ualberta.ca>
2021-07-15 14:23:12 -0600

    progress

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,18 @@
 module.exports = {
   purge: [
     "./source/js/**/*.{js,jsx}",
-    "./maintenance/maintenance.html",
     "./network-api/networkapi/wagtailpages/templates/**/*.html",
   ],
   mode: "jit",
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
+    screen: {
+      sm: "576px",
+      md: "768px",
+      lg: "992px",
+      xl: "1200px",
+    },
   },
   variants: {
     extend: {},
